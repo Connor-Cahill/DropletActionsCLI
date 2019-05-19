e1fbf2981d8541ffdce065c26808d3a31b7ba320
Connor Cahill <connorcahill913@gmail.com>
2019-05-18 22:20:10 -0700

    [Adds]: Create function to create new droplet

--- dropletActions/dropletactions.go
+++ dropletActions/dropletactions.go
@@ -25,3 +25,37 @@ func Index(client *godo.Client) ([]godo.Droplet, error) {
 
 	return droplets, nil
 }
+
+// Create creates new digital ocean droplet must input given params:
+// Name: the name of droplet
+// TODO: give region options to user
+// Region: region droplet will be located
+// size: size / money of droplet
+// image: build of droplet
+// SSHKeys: what ssh keys will be configged with droplet
+func Create(client *godo.Client, name string, region string, size string, image godo.DropletCreateImage, SSHKeys []godo.DropletCreateSSHKey) (string, error) {
+	// request to create new droplet
+	dropletRequest := &godo.DropletCreateRequest{
+		Name:    name,
+		Region:  region,
+		Size:    size,
+		Image:   image,
+		SSHKeys: SSHKeys,
+		IPv6:    false,
+	}
+
+	// create context
+	ctx := context.TODO()
+
+	droplet, _, err := client.Droplets.Create(ctx, dropletRequest)
+	if err != nil {
+		return "", err
+	}
+
+	publicIP, err := droplet.PublicIPv4()
+	if err != nil {
+		return "", err
+	}
+
+	return publicIP, nil
+}
