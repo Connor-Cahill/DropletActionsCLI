19d7badb92a8535ae0bc1ce345b8a1e1b14d6525
Connor Cahill <connorcahill913@gmail.com>
2019-05-18 22:46:45 -0700

    [Adds]: to Create function in dropletactions package

--- dropletActions/dropletactions.go
+++ dropletActions/dropletactions.go
@@ -33,7 +33,42 @@ func Index(client *godo.Client) ([]godo.Droplet, error) {
 // size: size / money of droplet
 // image: build of droplet
 // SSHKeys: what ssh keys will be configged with droplet
-func Create(client *godo.Client, name string, region string, size string, image godo.DropletCreateImage, SSHKeys []godo.DropletCreateSSHKey) (string, error) {
+func Create(client *godo.Client, name string) (string, error) {
+
+	// uses Ubuntu Droplet image
+	image := godo.DropletCreateImage{
+		Slug: "ubuntu-16-04-x64",
+	}
+
+	// size is to the $5/month plan by defualt currently
+	// can change later but need to give users way to easily input
+	size := "s-1vcpu-1gb"
+
+	// currently have region set to SF2
+	region := "sf2"
+
+	// NOTE: it currently connects all SSH keys on your account to the droplet
+	// I will fix this later but for right now this is how I use DO anyway
+	// options for ssh request
+	sshOpt := &godo.ListOptions{
+		Page:    1,
+		PerPage: 200,
+	}
+	ctx := context.TODO()
+	keys, _, err := client.Keys.List(ctx, sshOpt)
+	if err != nil {
+		return "", err
+	}
+
+	// list of SSH IDs to add to new droplet
+	SSHKeys := []godo.DropletCreateSSHKey{}
+	for _, key := range keys {
+		sshKey := godo.DropletCreateSSHKey{
+			ID: key.ID,
+		}
+		SSHKeys = append(SSHKeys, sshKey)
+	}
+
 	// request to create new droplet
 	dropletRequest := &godo.DropletCreateRequest{
 		Name:    name,
@@ -42,10 +77,8 @@ func Create(client *godo.Client, name string, region string, size string, image 
 		Image:   image,
 		SSHKeys: SSHKeys,
 		IPv6:    false,
+		Backups: false,
 	}
-
-	// create context
-	ctx := context.TODO()
 
 	droplet, _, err := client.Droplets.Create(ctx, dropletRequest)
 	if err != nil {
