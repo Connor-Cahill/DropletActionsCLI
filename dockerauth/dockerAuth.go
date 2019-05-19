bb5817edb87db770718e0fdfff5b750b2b937436
Connor Cahill <connorcahill913@gmail.com>
2019-05-18 21:39:01 -0700

    [Adds]: dockerauth package to return client object anywhere

---
+++ dockerauth/dockerAuth.go
@@ -0,0 +1,41 @@
+package dockerauth
+
+import (
+	"context"
+	"os"
+
+	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
+)
+
+// TokenSource struct holds the
+// Digital Ocean Personal Access Token
+type TokenSource struct {
+	AccessToken string
+}
+
+// Token Auth setup taken from docs
+// https://github.com/digitalocean/godo
+// auths user using digital ocean access token
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
+
+// Auth uses DO Personal Access Token and returns
+// authenticated client to then use with
+// Droplet automation commands
+func Auth() *godo.Client {
+	tokenSource := &TokenSource{
+		AccessToken: os.Getenv("DIGITAL_OCEAN_KEY"),
+	}
+
+	oauthClient := oauth2.NewClient(context.Background(), tokenSource)
+	// returing authenticated client object
+	client := godo.NewClient(oauthClient)
+
+	return client
+
+}
