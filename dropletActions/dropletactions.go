cd2f097da3dd99498ea85ab93f785da621a611eb
Connor Cahill <connorcahill913@gmail.com>
2019-05-19 00:06:19 -0700

    [Cleans]: droplet actions file

--- dropletActions/dropletactions.go
+++ dropletActions/dropletactions.go
@@ -2,7 +2,6 @@ package dropletactions
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/digitalocean/godo"
 )
@@ -32,7 +31,7 @@ func Index(client *godo.Client) ([]godo.Droplet, error) {
 // size: size / money of droplet
 // image: build of droplet
 // SSHKeys: what ssh keys will be configged with droplet
-func Create(client *godo.Client, name string) (string, error) {
+func Create(client *godo.Client, name string) error {
 
 	// uses Ubuntu Droplet image
 	image := godo.DropletCreateImage{
@@ -56,7 +55,7 @@ func Create(client *godo.Client, name string) (string, error) {
 	ctx := context.TODO()
 	keys, _, err := client.Keys.List(ctx, sshOpt)
 	if err != nil {
-		return "", err
+		return err
 	}
 
 	// list of SSH IDs to add to new droplet
@@ -79,13 +78,12 @@ func Create(client *godo.Client, name string) (string, error) {
 		Backups: false,
 	}
 
-	droplet, _, err := client.Droplets.Create(ctx, dropletRequest)
+	_, _, err = client.Droplets.Create(ctx, dropletRequest)
 	if err != nil {
-		return "", err
+		return err
 	}
 
-	fmt.Println("DROPLET: ", droplet)
-	return "Droplet Created", nil
+	return nil
 }
 
 // Get returns existing droplet given droplet ID
