0f3033e6b7e5959b6a82923b6d60ca11243af9d0
Connor Cahill <connorcahill913@gmail.com>
2019-05-20 12:32:04 -0700

    [Fixes]: scp script in dropletActions for copyEnv. env can now be copied onto droplet

--- dropletActions/dropletactions.go
+++ dropletActions/dropletactions.go
@@ -157,7 +157,7 @@ func GetProject(ip string, projectLink string, dirName string) error {
 // Must pass path to .env file as argument and DO Droplet IP
 func CopyEnv(pathToEnv string, ip string, dirName string) error {
     // command to secure copy .env into VPS
-    cmd := exec.Command("bash", "-c", "scp "+pathToEnv+" root@"+ip+":$HOME/"+dirName)
+    cmd := exec.Command("bash", "-c", "scp "+pathToEnv+" root@"+ip+":/root/"+dirName+"/")
     // return output of command (error message included)
     // using for debugging purposes
     output, err := cmd.CombinedOutput()
