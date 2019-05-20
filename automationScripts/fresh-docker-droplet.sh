f19e7cc8076bcc5a02fff8ce8645eb604ccdc9f6
Connor Cahill <connorcahill913@gmail.com>
2019-05-19 18:12:26 -0700

    [Edits]: bash script

--- automationScripts/fresh-docker-droplet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Starting Setup Script..."
-curl -fsSL https://get.docker.com -o get-docker.sh \
-        && sh get-docker.sh \
-        && apt-get update && apt-get install -y docker-compose
