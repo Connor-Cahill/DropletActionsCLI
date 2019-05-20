e17913f42cf21da8f2e2ac4bd5a3f2ece0594bad
Connor Cahill <connorcahill913@gmail.com>
2019-05-20 13:18:33 -0700

    [Adds]: CLI command for downCompose droplet action

---
+++ cmd/downCompose.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+
+	dropletactions "github.com/connor-cahill/dropletAutomationCLI/dropletActions"
+	"github.com/spf13/cobra"
+)
+
+// DownComposeCmd will ssh into DO Droplet and run docker-compose down on compose file
+// to stop docker-compose instance on droplet
+// takes in DO Droplet public IP and path to compose file as arguments
+var DownComposeCmd = &cobra.Command{
+	Use:   "downCompose",
+    Short: "Will ssh into DO Droplet and docker-compose down stopping docker-compose instance. Must pass following args IN ORDER:\n1) Droplet public IP\n2) Path to compose file\n",
+	Run: func(cmd *cobra.Command, args []string) {
+	    // get CLI Args
+        dropletIP := args[0]
+        pathToCompose := args[1]
+
+        // run dropletactions command
+        err := dropletactions.StartCompose(dropletIP, pathToCompose)
+        if err != nil {
+            log.Fatalln("Error running commands on Droplet: ", err)
+        }
+
+        // no error command was successful
+        fmt.Println("Command successful. Docker compose instance was stopped.")
+        
+	    
+	},
+}
+
+// Mounts the command onto the root command
+func init() {
+	RootCmd.AddCommand(DownComposeCmd)
+}
