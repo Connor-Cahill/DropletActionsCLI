75e4212ac110fe7ddde9e000b8ffd01f57f5bb1a
Connor Cahill <connorcahill913@gmail.com>
2019-05-25 15:58:41 -0700

    [Adds]: logic to find main dir of user and setup db connection

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"log"
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/connor-cahill/dropletActionsCLI/cmd"
 	"github.com/joho/godotenv"
 	"github.com/connor-cahill/dropletActionsCLI/dropletauth"
+	"github.com/boltdb/bolt"
+	homedir "github.com/mitchellh/go-homedir"
 )
 
 // Init function will load in env vars from .env file
@@ -20,6 +23,16 @@ func init() {
 }
 
 func main() {
+    
+    // find users home directory
+    home, err := homedir.Dir()
+    if err != nil {
+        log.Fatalln("Error setting up DB: ", err)
+    }
+
+    // set path to db in users home dir
+    dbPath := filepath.Join(home, "dropletActions.db")
+
 
     // if no DO API Key set ask for it
     if os.Getenv("DIGITAL_OCEAN_KEY") == "" {
@@ -30,7 +43,7 @@ func main() {
 	// Execute the CLI package
 	// and mount the root command
 	// TODO: how can I pass the client obj to the CLI?
-	err := cmd.RootCmd.Execute()
+	err = cmd.RootCmd.Execute()
 	if err != nil {
 		log.Fatalln("Error starting the CLI: ", err)
 	}
